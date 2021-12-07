861ed96dc72aec335e663910efaa43ac101af558
Cameron Fiorato <cameronclayfiorato@gmail.com>
2021-12-07 12:45:01 -0600

    Fixed Modal Functionality

--- src/index.js
+++ src/index.js
@@ -1,52 +1,104 @@
+//test
 document.addEventListener('DOMContentLoaded', () => {
-  fetchQuestion()
+  fetchQuestions()
 })
-let answer
-function fetchQuestion() {
-  fetch("https://opentdb.com/api.php?amount=1&category=9&difficulty=easy&type=boolean")
+let userAnswer
+let defaultQuestions = []
+let currentQuestionData = {
+  question: "",
+  answer: "",
+  questionID: ""
+}
+const fetchQuestions = () => {
+  fetch("http://localhost:3000/results")
   .then(resp => resp.json())
-  .then(data => {
-    let question = data.results[0].question
-    answer = data.results[0].correct_answer
-    let h2 = document.createElement('h2')
-    h2.textContent = question
-    document.querySelector('#current-question').append(h2)
+  .then(questions => {
+    questions.forEach(question => {
+      defaultQuestions.push(question)
+    })
+    loadQuestions(defaultQuestions)
+  }) 
+}
+
+function loadQuestions(questionList) {
+  questionList.forEach(obj => {
+    console.log(obj.question)
+    let li = document.createElement('li')
+    li.innerText = `Question ${obj.questionID}`
+    li.id = obj.questionID
+    li.addEventListener('click', (e) => {
+      defaultQuestions.forEach(question => {
+        if (e.target.id == question.questionID){
+          currentQuestionData.question = question.question
+          currentQuestionData.answer = question.correct_answer
+          currentQuestionData.questionID = question.questionID
+          loadQuestion(question.question)
+        }
+      })
+    })
+    document.querySelector('#question-list').appendChild(li)
   })
 }
-const trueModal = document.querySelector('#true-modal')
-const falseModal = document.querySelector("#false-modal")
-let userAnswer
 
-document.querySelector('button.true').addEventListener('click', (e) => {
-  console.log(e.target.innerText)
-  userAnswer = e.target.innerText
-  getResults(userAnswer)
-})
-
-document.querySelector('button.false').addEventListener('click', (e) => {
-  console.log(e.target.innerText)
-  userAnswer = e.target.innerText
-  getResults(userAnswer)
-})
-
-function getResults(userAnswer){
-  if (userAnswer == answer){
-    console.log("correct!")
-    trueModal.style.display = 'block'
-  }else if (userAnswer !== answer){
-    console.log("incorrect!")
-    falseModal.style.display = 'block'
-  }else{
-    console.log("answer error!")
-  }
+function loadQuestion(question){
   document.querySelector('#current-question').innerHTML = ''
-  fetchQuestion()
+  let h2 = document.createElement('h2')
+  h2.id = "current"
+  h2.innerText = question
+  document.querySelector('#current-question').appendChild(h2)
 }
 
-document.getElementsByClassName("closeTrue")[0].addEventListener('click', () => {
-  trueModal.style.display = 'none'
+document.querySelector('#random').addEventListener('click', () => {
+  getRandomQuestion()
 })
 
-document.getElementsByClassName("closeFalse")[0].addEventListener('click', () => {
-  falseModal.style.display = 'none'
+function getRandomQuestion() {
+  fetch("https://opentdb.com/api.php?amount=1&difficulty=easy&type=boolean")
+  .then(resp => resp.json())
+  .then(data => {
+    let randomQuestion = data.results[0].question
+    let correctAnswer = data.results[0].correct_answer
+    currentQuestionData.question = randomQuestion
+    currentQuestionData.answer = correctAnswer
+    loadQuestion(randomQuestion)
+  })
+}
+document.querySelector('#answer-select #true').addEventListener('click', (e) => {
+  userAnswer = e.target.innerText
+  getResult()
+})
+document.querySelector('#answer-select #false').addEventListener('click', (e) => {
+  userAnswer = e.target.innerText
+  getResult()
+})
+
+function getResult() {
+  let response
+  if (userAnswer === currentQuestionData.answer){
+    console.log('Correct!')
+    response = 'Correct!'
+  }else if (userAnswer !== currentQuestionData.answer){
+    console.log('Incorrect!')
+    response = 'Incorrect!'
+  }else{
+    console.log('Error!')
+  }
+  handleAnswer(response)
+}
+
+function handleAnswer(response){
+  let modalContainer = document.querySelector('#modalContainer')
+  let modal = document.querySelector('#modal')
+  let h1 = document.createElement('h1')
+  h1.id = 'feedback'
+  h1.textContent = response
+  let p = document.querySelector('#directions')
+  modal.insertBefore(h1, p)
+  modalContainer.classList.add('show')
+}
+
+document.querySelector('#close').addEventListener('click', () => {
+  let h1 = document.querySelector('#feedback')
+  h1.remove()
+  document.querySelector('#modalContainer').classList.remove('show')
 })
---
+++ src/working.js
@@ -0,0 +1,39 @@
+document.addEventListener('DOMContentLoaded', () => {
+  fetchQuestions()
+})
+let answer
+function randomQuestion() {
+  fetch("https://opentdb.com/api.php?amount=1&category=9&difficulty=easy&type=boolean")
+  .then(resp => resp.json())
+  .then(data => {
+    let question = data.results[0].question
+    answer = data.results[0].correct_answer
+    let h2 = document.createElement('h2')
+    h2.textContent = question
+    document.querySelector('#current-question').append(h2)
+  })
+}
+
+let userAnswer
+
+document.querySelector('button.true').addEventListener('click', (e) => {
+  console.log(e.target.innerText)
+  userAnswer = e.target.innerText
+  getResults(userAnswer)
+})
+
+document.querySelector('button.false').addEventListener('click', (e) => {
+  console.log(e.target.innerText)
+  userAnswer = e.target.innerText
+  getResults(userAnswer)
+})
+
+function getResults(userAnswer){
+  if (userAnswer == answer){
+    console.log("correct!")
+  }else if (userAnswer !== answer){
+    console.log("incorrect!")
+  }else{
+    console.log("answer error!")
+  }
+}
